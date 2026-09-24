860daaf1b60a367cc78e945874e24dc3138c31b5
alkhms101 <alk@hms.de>
2026-09-24 11:30:57 +0200

    A7077A-139 Trivial wording errors in abcc_timer.h

--- src/abcc_timer.h
+++ src/abcc_timer.h
@@ -3,7 +3,7 @@
 ** Licensed under the MIT License.
 ********************************************************************************
 ** File Description:
-** Implements abcc timer service.
+** Implements the ABCC timer service.
 ********************************************************************************
 */
 
@@ -22,18 +22,18 @@
 typedef void (*ABCC_TimerTimeoutCallbackType)( void );
 
 /*
-** Type for identifying timer
+** Type for identifying a timer.
 */
 typedef UINT8 ABCC_TimerHandle;
 
 /*------------------------------------------------------------------------------
-** void ABCC_TimerInit( void );
-** Need to called before the timer is used
+** Need to be called before the timer can be used.
 **------------------------------------------------------------------------------
 ** Arguments:
-**    None
+**    None.
+**
 ** Returns:
-**    None
+**    None.
 **------------------------------------------------------------------------------
 */
 EXTFUNC void ABCC_TimerInit( void );
@@ -42,27 +42,28 @@ EXTFUNC void ABCC_TimerInit( void );
 ** Allocates a timer resource and returns a handle.
 **------------------------------------------------------------------------------
 ** Arguments:
-**    pnHandleTimeout: Function to call if timeout.
+**    pnHandleTimeout  -  Function to call if timeout.
+**
 ** Returns:
-**    ABCC_TimerHandle ( Used as identifier when using timer functions. )
-**                       TIMER_NO_HANDLE is returned if no timer was available
+**    ABCC_TimerHandle - ( Used as identifier when using timer functions. )
+**                       TIMER_NO_HANDLE is returned if no timer was available.
 **------------------------------------------------------------------------------
 */
 EXTFUNC ABCC_TimerHandle ABCC_TimerCreate( ABCC_TimerTimeoutCallbackType pnHandleTimeout );
 
 /*------------------------------------------------------------------------------
-** Start timer.
-** When the timeout is reached, the registered callback function is called.
-** Note!! This function is dependent on that ABCC_TimerTick() is called
-** on regular basis.
+** Starts the timer.
+** When the timeout expires, the registered callback function is called.
+** Note!! This function depends on ABCC_TimerTick() being called
+** on a regular basis.
 **------------------------------------------------------------------------------
 ** Arguments:
-**    xHandle:     Identifier of timer to be started.
-**    lTimeoutMs:  Timeout in ms.
+**    xHandle    - Identifier of the timer to be started.
+**    lTimeoutMs - Timeout in ms.
 **
 ** Returns:
-**    TRUE if the timer had expired before re-start.
-**    FALSE Timer had not expired before re-start.
+**    TRUE  - Timer expired before the restart.
+**    FALSE - Timer did not expire before the restart.
 **------------------------------------------------------------------------------
 */
 EXTFUNC BOOL ABCC_TimerStart( ABCC_TimerHandle xHandle,
@@ -72,33 +73,36 @@ EXTFUNC BOOL ABCC_TimerStart( ABCC_TimerHandle xHandle,
 ** Stop timer.
 **------------------------------------------------------------------------------
 ** Arguments:
-**    xHandle: Identifier of timer to be stopped.
+**    xHandle - Identifier of the timer to be stopped.
+**
 ** Returns:
-**    FALSE If timeout is already reached
-**    TRUE: Timer stopped OK
+**    TRUE  - Timer stopped OK.
+**    FALSE - Timeout is already reached.
 **------------------------------------------------------------------------------
 */
 EXTFUNC BOOL ABCC_TimerStop( ABCC_TimerHandle xHandle );
 
 /*------------------------------------------------------------------------------
-** ABCC_TimerTick(). Provides delta time since last timer tick call.
+** Provides delta time since last timer tick call.
 ** Typically called from timer interrupt.
 **------------------------------------------------------------------------------
 ** Arguments:
-**    iDeltaTimeMs: Time in ms since last timerTick call
+**    iDeltaTimeMs - Time in ms since last timerTick call.
+**
 ** Returns:
-**    None
+**    None.
 **------------------------------------------------------------------------------
 */
 EXTFUNC void ABCC_TimerTick( const INT16 iDeltaTimeMs );
 
 /*------------------------------------------------------------------------------
-** ABCC_TimerDisable(). Disable tick action
+** Disable tick action.
 **------------------------------------------------------------------------------
 ** Arguments:
-**    None
+**    None.
+**
 ** Returns:
-**    None
+**    None.
 **------------------------------------------------------------------------------
 */
 EXTFUNC void ABCC_TimerDisable( void );
@@ -107,7 +111,8 @@ EXTFUNC void ABCC_TimerDisable( void );
 ** Get number of ticks counter by the timer system since start.
 **------------------------------------------------------------------------------
 ** Arguments:
-**    None
+**    None.
+**
 ** Returns:
 **    Total number of ticks counted by the timer system.
 **------------------------------------------------------------------------------
